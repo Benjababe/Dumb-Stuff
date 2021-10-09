ab3a2178984dae1238a55a89856fd9be7c2d0175
Benjababe <bengohzy@gmail.com>
2021-10-09 09:14:15 +0800

    Added made userscripts

---
+++ Userscripts/google-logo-replacer.js
@@ -0,0 +1,55 @@
+// ==UserScript==
+// @name         Chin Fast Image Downloader
+// @namespace    https://greasyfork.org/en/users/86284-benjababe
+// @version      1.04
+// @description  One click to download current hovered image/webmeme
+// @author       Benjababe
+
+// @match        https://boards.4channel.org/*
+// @match        https://arch.b4k.co/*
+// @match        https://archived.moe/*
+// @match        https://archive.nyafuu.org/*
+// @match        https://archive.wakarimasen.moe/*
+// @match        https://desuarchive.org/*
+// @match        https://warosu.org/*
+
+// @grant        GM_download
+// ==/UserScript==
+
+// jshint esversion: 6
+
+(function () {
+    'use strict';
+
+    const HOTKEY = "Pause";
+
+    document.onkeydown = (e) => {
+        // key can be whatever you want, I choose pause as it's what I bound my mouse side keys to
+        if (e.code === HOTKEY) {
+            // get all elements hovered
+            let els = document.querySelectorAll(":hover");
+            els.forEach((el) => {
+                // only download for images
+                if (el.tagName.toLowerCase() === "img") {
+                    // link to original image/webmeme usually is the parent <a> element
+                    let parent = el.parentNode,
+                        url = parent.href,
+                        filename = HDFilenameFromURL(url);
+                    GM_download(url, filename);
+                }
+            });
+        }
+    }
+
+    // eg. ".../1622014662736s.jpg -> 1622014662736.jpg"
+    let HDFilenameFromURL = (url) => {
+        let SDFilename = url.split("/").pop();
+        SDFilename = SDFilename.split(".");
+
+        if (SDFilename[0][SDFilename[0].length - 1] == "s") {
+            SDFilename[0] = SDFilename[0].slice(0, -1);
+        }
+
+        return SDFilename.join(".");
+    }
+})();
---
+++ Userscripts/ntu-kaltura-downloader.js
@@ -0,0 +1,146 @@
+// ==UserScript==
+// @name         NTU Kaltura Downloader
+// @namespace    https://greasyfork.org/en/users/86284-benjababe
+// @version      1.1.5
+// @description  Fuck kaltura. It's shit
+// @author       Benjababe
+// @match        https://ntulearn.ntu.edu.sg/webapps/*
+// @match        https://ntulearnv.ntu.edu.sg/media/*
+// @match        https://ntulearnv.ntu.edu.sg/playlist/dedicated/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    const sourceTemplate = "https://api.sg.kaltura.com/p/117/sp/11700/playManifest/entryId/%ENTRY_ID_HERE%/format/download/protocol/https/flavorParamIds/0",
+        entryPrefix = "/entry_id/";
+
+    window.onload = () => {
+        // regular blackboard page with embedded player
+        if (location.pathname.indexOf("/webapps/blackboard/") == 0) {
+            genBlackBoard();
+        }
+
+        // kaltura single media
+        if (location.pathname.includes("/media/t/")) {
+            genKPage(false);
+        }
+
+        // kaltura playlist
+        if (location.pathname.includes("/playlist/dedicated/")) {
+            genKPage(true);
+        }
+    }
+
+    let genBlackBoard = () => {
+        console.log("NTU Kaltura Downloader running...");
+
+        genEmbeddedVideos();
+        genLinks();
+    };
+
+
+    // generates download button for embedded kaltura videos in blackboard page (eg. CZ1106)
+    let genEmbeddedVideos = () => {
+        let iframes = Array.from(document.getElementsByTagName("iframe"));
+        iframes.forEach((iframe) => {
+            let src = iframe.src,
+                sIndex = src.indexOf(entryPrefix);
+
+            //stops if it isn't a kultura video iframe
+            if (sIndex != -1) {
+                let eidIndex = src.indexOf(entryPrefix) + entryPrefix.length;
+                src = src.substring(eidIndex);
+                let entryID = src.substring(0, src.indexOf("/")),
+                    sourceURL = sourceTemplate.replace("%ENTRY_ID_HERE%", entryID);
+
+                appendDownloadButton(iframe, sourceURL);
+            }
+        });
+    };
+
+    // generated download button for <a> tags to kaltura videos in blackboard page (eg. CZ2002)
+    let genLinks = () => {
+        let links = Array.from(document.querySelectorAll("a[target]"));
+        links.forEach((link) => {
+            if (link.href.indexOf("https://api.sg.kaltura.com") == 0) {
+                let url = link.href,
+                    entryID = url.split("entry_id=")[1],
+                    sourceURL = sourceTemplate.replace("%ENTRY_ID_HERE%", entryID);
+
+                appendDownloadButton(link, sourceURL);
+            }
+        });
+    };
+
+
+    // generates download button in the kaltura media gallery (eg. CZ2005)
+    let genKPage = (playlist = false) => {
+        let path = location.pathname, entryID, sourceURL,
+            details = document.querySelector("span.data, span.first");
+
+        let pathSpl = path.replace("/media/t/", "").split("/");
+
+        if (playlist) {
+            entryID = pathSpl[pathSpl.length - 1];
+        }
+
+        else {
+            entryID = pathSpl[0];
+        }
+
+        sourceURL = sourceTemplate.replace("%ENTRY_ID_HERE%", entryID);
+        listenDOM(sourceURL);
+        console.log(location.href, sourceURL);
+    };
+
+
+    // appends download button with sourceURL to whatever element is passed through sibling argument
+    let appendDownloadButton = (sibling, sourceURL) => {
+        let dlBtn = document.createElement("button");
+        dlBtn.innerText = "Download";
+        dlBtn.style.position = "absolute";
+        dlBtn.style.userSelect = "none";
+        dlBtn.style.marginLeft = "10px";
+        dlBtn.onclick = (e) => { window.location.href = sourceURL; e.target.blur(); };
+
+        sibling.parentNode.appendChild(dlBtn);
+    };
+
+
+    // appends download to action list when it is detected in MutationObserver
+    // whole page doesn't load in at once in this scenario
+    let listenDOM = (sourceURL) => {
+        let observer = new window.MutationObserver(function (mutations, observer) {
+            mutations.forEach((mutation) => {
+                if (mutation.target.id == "entryActions") {
+                    let ul = mutation.target.querySelector("ul#entryActionsMenu"),
+                        li = document.createElement("li"),
+                        a = document.createElement("a"),
+                        span = document.createElement("span");
+
+                    span.innerText = "Download";
+                    a.appendChild(span);
+                    a.href = sourceURL;
+                    li.appendChild(a);
+                    li.setAttribute("role", "presentation");
+                    ul.appendChild(li);
+                }
+            });
+        });
+
+        observer.observe(document, {
+            subtree: true,
+            attributes: true
+        });
+    }
+
+})();
+
+
+
+
+
+
+
+
+
